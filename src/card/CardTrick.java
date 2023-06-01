ffa082ce503d0b3668976d5be6c121b2f65d774c
shar1348 <134955482+shar1348@users.noreply.github.com>
2023-05-31 20:43:51 -0400

    UpdatedCardTrick.java

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -30,10 +30,16 @@ public class CardTrick {
             magicHand[i] = c;
         }
 
+        //  luckyCard is added 
+        Card luckyCard = new Card();
+        luckyCard.setValue(7);
+        luckyCard.setSuit(Card.SUITS[1]);
+
+
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the value of your card: ");
         int value = scanner.nextInt();
-        System.out.print("Enter the suit of your card (0-3 where 0 = Hearts, 1 = Diamonds, 2 = Clubs, 3 = Spades): ");
+        System.out.print("Enter the suit of your card (0-3): ");
         int suit = scanner.nextInt();
 
         Card userCard = new Card();
@@ -49,17 +55,26 @@ public class CardTrick {
         }
 
         if(found) {
-            System.out.println("hurrah! Your card is in the magic hand.");
+            System.out.println("Hurrah! Your card is in the magic hand.");
         } else {
             System.out.println("Alas! Your card is not in the magic hand.");
+        }
+        if(luckyCard.getValue() == userCard.getValue() && luckyCard.getSuit().equals(userCard.getSuit())) 
+        {
+            System.out.println("You have also found the lucky card!");
+        }
+        else 
+        {
+            System.out.println("You have NOT found the lucky card!");
         }
     }
 
     private static int generateRandomNumber() {
         return (int) (Math.random() * 13 + 1);
     }
-
+    
     private static int generateRandomNumber(int min, int max) {
         return (int) (Math.random() * (max - min + 1) + min);
     }
+    
 }
